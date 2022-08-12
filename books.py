44dc0352426c16dc1dd3b81ded7f7c8ff249fd8f
Neelesh <neelesh.vishwakarma235@gmail.com>
2022-08-12 18:04:05 +0530

    CRUD operation API done

--- books.py
+++ books.py
@@ -2,6 +2,7 @@ from typing import Optional
 from unittest import async_case
 from fastapi import FastAPI
 from enum import Enum
+import uvicorn
 
 app = FastAPI()
 
@@ -40,3 +41,33 @@ def read_fav_book():
 @app.get("/book/{book_id}")
 def read_book(book_id: int):
     return {"book_title" : book_id}
+
+@app.post("/create_book")
+def create_book(book_title, book_author):
+
+    current_book_id = 0
+
+    if len(books) > 0:
+
+        for book in books:
+            current_book_id = int(book.split('_')[-1])
+            
+        books['book_' + str(current_book_id + 1)] = {"title" : book_title, "author" : book_author}
+    
+    return books['book_' + str(current_book_id + 1)]
+
+
+@app.put("/update_book/{book_name}")
+def update_book(book_name: str, book_title: str, book_author: str):
+    books[book_name] = {"title": book_title, "author": book_author}
+    return books[book_name]
+
+@app.delete("/delete_book/{book_name}")
+def delete_book(book_name: str):
+    del books[book_name]
+    return f'{book_name} deleted'
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("books:app", host='0.0.0.0', port=8000, reload=True)
