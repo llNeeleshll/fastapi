542cce523097e9312facbc39e2be20a1b6968568
Neelesh <neelesh.vishwakarma235@gmail.com>
2022-08-12 14:06:00 +0530

    Optional query parameter added

--- books.py
+++ books.py
@@ -1,5 +1,7 @@
+from typing import Optional
 from unittest import async_case
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,9 +13,29 @@ books = {
     "book_5" : {"title" : "Title Five", 'author' : "Author Five"}
 }
 
+class DirectionName(str, Enum):
+    north = "North"
+    south = "South"
+    east = "East"
+    west = "West"
+
 @app.get("/")
-async def read_all_books():
-    return books
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = books.copy()
+        del new_books[skip_book]
+        return new_books
+    else:
+        return books
+
+@app.get("/directions/{direction_name}")
+def get_directions(direction_name : DirectionName):
+    return {"message" : "This"}
+
+@app.get("/book/mybook")
+def read_fav_book():
+    return {"book_title" : "My Book"}
+
 
 @app.get("/book/{book_id}")
 def read_book(book_id: int):
